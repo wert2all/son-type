5624977ba7515621f430a0c10bf89b512ddd84cc
Serhii Shvets <wert2all@gmail.com>
2025-03-11 02:43:48 +0200

    feat: add component for typing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./features/typer/typer.component').then(t => t.TyperComponent),
+  },
+];
