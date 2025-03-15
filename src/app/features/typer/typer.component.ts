f3bcb841d5a13b93deae159a0d38f950580ab24e
wert2all <wert2all@gmail.com>
2025-03-15 16:10:01 +0200

    feat: add mask selector

--- src/app/features/typer/typer.component.ts
+++ src/app/features/typer/typer.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { Component, signal } from '@angular/core';
+import { provideIcons } from '@ng-icons/core';
 import { phosphorPassword } from '@ng-icons/phosphor-icons/regular';
 import { TyperInputComponent } from './input/input.component';
+import { TyperMenuComponent } from './menu/menu.component';
+import { TyperMask } from './typer.types';
+
 @Component({
   standalone: true,
   selector: 'app-typer-container',
   templateUrl: './typer.component.html',
-  imports: [TyperInputComponent, NgIconComponent],
+  imports: [TyperInputComponent, TyperMenuComponent],
   viewProviders: [provideIcons({ phosphorPassword })],
 })
-export class TyperContainerComponent {}
+export class TyperContainerComponent {
+  masks = signal<TyperMask[]>([{ title: 'numbers', mask: '0123456789' }]);
+  currentMask = signal<TyperMask | null>(null);
+}
