16a26c7d8d3be31c84290ca1bcabcaf5212a32b0
Serhii Shvets <wert2all@gmail.com>
2025-03-21 23:16:23 +0200

    feat: create settings

--- src/app/features/typer/typer.types.ts
+++ src/app/features/typer/typer.types.ts
@@ -6,3 +6,31 @@ export interface TyperSymbol {
   symbol: string;
   isError: boolean;
 }
+
+export interface TyperSettings {
+  masks: Record<TyperMaskName, TyperMask>;
+  activeMask: TyperMaskName;
+  count: number;
+  letters: number;
+}
+
+export type TyperMaskName = keyof typeof TYPER_MASKS;
+
+export const TYPER_MASKS: Record<string, TyperMask> = {
+  numbers: {
+    title: 'numbers',
+    mask: '0123456789',
+  },
+  'number worlds': {
+    title: 'number worlds',
+    mask: '0123456789 ',
+  },
+  letters: {
+    title: 'letters',
+    mask: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  },
+  worlds: {
+    title: 'worlds',
+    mask: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ ',
+  },
+};
