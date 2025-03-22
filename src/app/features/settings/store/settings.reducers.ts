08ae0cfb06dc83c84a0a7edc88d009da94f0c23f
Serhii Shvets <wert2all@gmail.com>
2025-03-22 13:24:29 +0200

    feat: saveing setting in state

--- src/app/features/settings/store/settings.reducers.ts
+++ src/app/features/settings/store/settings.reducers.ts
@@ -1,12 +1,27 @@
-import { createFeature, createReducer } from '@ngrx/store';
+import { createFeature, createReducer, on } from '@ngrx/store';
+import { TYPER_MASKS } from '../../typer/typer.types';
+import { SettingsActions } from './settings.actions';
 import { SettingsState } from './settings.types';
 
 const initState: SettingsState = {
   typer: {
+    masks: TYPER_MASKS,
+    activeMask: 'numbers',
     count: 10,
+    letters: 5,
   },
 };
 export const settingsFeature = createFeature({
   name: 'settings',
-  reducer: createReducer(initState),
+  reducer: createReducer(
+    initState,
+
+    on(
+      SettingsActions.saveSettings,
+      (state, { typer }): SettingsState => ({
+        ...state,
+        typer: typer,
+      })
+    )
+  ),
 });
--- src/app/features/settings/store/settings.types.ts
+++ src/app/features/settings/store/settings.types.ts
@@ -1,7 +1,13 @@
+import { TyperMask, TyperMaskName } from '../../typer/typer.types';
+
+
 export interface SettingsState {
   typer: TyperSettings;
 }
 
 export interface TyperSettings {
+  masks: Record<TyperMaskName, TyperMask>;
+  activeMask: TyperMaskName;
   count: number;
+  letters: number;
 }
