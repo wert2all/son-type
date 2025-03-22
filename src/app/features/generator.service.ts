17e6fa499e2945753fed976abeab6047e976b6c8
Serhii Shvets <wert2all@gmail.com>
2025-03-22 18:13:05 +0200

    feat: separate to words

--- src/app/features/generator.service.ts
+++ src/app/features/generator.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@angular/core';
 
+const WORD_SEPARATOR = ' ';
+
 @Injectable({ providedIn: 'root' })
 export class GeneratorService {
-  generate(count: number, mask: string): string[] {
+  generate(count: number, mask: string, maxWordLength = 0): string[] {
     const result = [];
     if (mask.length !== 0) {
       while (result.length < count) {
         // eslint-disable-next-line sonarjs/pseudo-random
         const randomIndex = Math.floor(Math.random() * mask.length);
         const ramdomChar = mask.charAt(randomIndex);
-
+        if (maxWordLength) {
+          const word = this.getWord(result);
+          if (word.length >= maxWordLength) {
+            result.push(WORD_SEPARATOR);
+          }
+        }
         result.push(ramdomChar);
       }
     }
 
     return result;
+  }
+  private getWord(result: string[]): string {
+    return result.join('').split(WORD_SEPARATOR).pop() || '';
   }
 }
