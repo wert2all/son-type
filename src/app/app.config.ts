6eb5d835a92420c30e6de4e09c0dc609b3982a74
Serhii Shvets <wert2all@gmail.com>
2025-03-22 12:50:51 +0200

    fix: add shared store to store provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,14 @@ import { provideRouter } from '@angular/router';
 import { provideStore } from '@ngrx/store';
 import { routes } from './app.routes';
 import { settingsFeature } from './features/settings/store/settings.reducers';
+import { sharedFeatures } from './shared/store/shared.reducers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideStore({ [settingsFeature.name]: settingsFeature.reducer }),
+    provideStore({
+      [settingsFeature.name]: settingsFeature.reducer,
+      [sharedFeatures.name]: sharedFeatures.reducer,
+    }),
   ],
 };
