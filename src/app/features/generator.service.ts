dfb567cbaea8b4e8b12502d79815378839e3b48d
Serhii Shvets <wert2all@gmail.com>
2025-03-22 17:47:01 +0200

    refactor: refactor generation of symbols for support words in future

--- src/app/features/generator.service.ts
+++ src/app/features/generator.service.ts
@@ -2,19 +2,18 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class GeneratorService {
-  generate(length: number, symbols: string): string[] {
-    if (length <= 0 || symbols.length === 0) {
-      return [];
-    }
-
+  generate(count: number, mask: string): string[] {
     const result = [];
-    for (let i = 0; i < length; i++) {
-      // eslint-disable-next-line sonarjs/pseudo-random
-      const randomIndex = Math.floor(Math.random() * symbols.length);
-      const ramdomChar = symbols.charAt(randomIndex);
+    if (mask.length !== 0) {
+      while (result.length < count) {
+        // eslint-disable-next-line sonarjs/pseudo-random
+        const randomIndex = Math.floor(Math.random() * mask.length);
+        const ramdomChar = mask.charAt(randomIndex);
 
-      result.push(ramdomChar);
+        result.push(ramdomChar);
+      }
     }
+
     return result;
   }
 }
