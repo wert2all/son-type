1b95dfea16e461e42392ce6a9af9d78852f32b36
Serhii Shvets <wert2all@gmail.com>
2025-03-22 12:55:40 +0200

    feat: add selectror for typer to shared store

--- src/app/shared/store/shared.reducers.ts
+++ src/app/shared/store/shared.reducers.ts
@@ -1,6 +1,13 @@
-import { createFeature, createReducer } from '@ngrx/store';
+import { createFeature, createReducer, createSelector } from '@ngrx/store';
+import { settingsFeature } from '../../features/settings/store/settings.reducers';
 
 export const sharedFeatures = createFeature({
   name: 'shared',
   reducer: createReducer({}),
+  extraSelectors: () => ({
+    selectTyperSettings: createSelector(
+      settingsFeature.selectTyper,
+      state => state
+    ),
+  }),
 });
